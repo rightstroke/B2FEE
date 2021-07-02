ab04336d06f85645b16c7a2d51877689d2696496
Siva <siva@gmail.com>
2021-07-02 17:04:22 +0530

    iv

--- react/myproj-forms/src/Components/UserRegistration.js
+++ react/myproj-forms/src/Components/UserRegistration.js
@@ -42,7 +42,7 @@ export default function UserRegistration(props) {
     const reqOptios = {
       method:"POST",
       headers : {
-        "Content-Type":"application/json"
+        "Content-Type":"application/json"   
       },
       body: JSON.stringify(formValues)
     };
